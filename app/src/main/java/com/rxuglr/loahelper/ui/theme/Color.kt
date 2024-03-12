e3116f4d85a7e97f698720adff9a8f50e398fce5
nni16 <you@example.com>
2024-03-12 22:55:13 +0300

    woa-loa

--- app/src/main/java/com/rxuglr/loahelper/ui/theme/Color.kt
+++ app/src/main/java/com/rxuglr/loahelper/ui/theme/Color.kt
@@ -2,12 +2,15 @@ package com.rxuglr.loahelper.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val BLACK: Color = Color(0xff3b5580)
-val BLACK_LIGHT: Color = Color(0xff70a5fa)
-val SECONDARY_LIGHT: Color = Color(0xff1e2b40)
+val BLACK: Color = Color(0xff9b320e)
+val BLACK_LIGHT: Color = Color(0xffdd4814)
+val SECONDARY_LIGHT: Color = Color(0xff6e240a)
 
 val BLACK_DARK: Color = Color(0xFF000000)
-val SECONDARY_DARK: Color = Color(0xff3b5580)
+val SECONDARY_DARK: Color = Color(0xff9b320e)
 
 // FF1D1D1D
 // FF000000
+
+
+//#9b320e
